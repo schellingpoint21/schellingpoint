9ae94590f3222fd9c29b0f68a12da5b396f34ae7
psycarlo <psycarlo1@gmail.com>
2026-01-31 20:07:42 +0000

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="font-bold text-blue-500">Hello</h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    'nuxt-studio'
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: '@/components/ui'
+  }
+})
